e52f83a67769033a76aa1b8ead67268cca1a9cde
yutaatamura <yutaatamura@gmail.com>
2018-07-03 09:52:07 -0500

    Update links

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-    // Create timeline
-    $('#experience-timeline').each(function() {
+(function($) {
+      // Animate to section when nav is clicked
+       $('#right-menu a').click(function(e) {
 
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
+        // Treat as normal link if no-scroll class
+        if ($(this).hasClass('no-scroll')) return;
 
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
+        e.preventDefault();
+        var heading = $(this).attr('href');
+        var scrollDistance = $(heading).offset().top;
 
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
+        $('html, body').animate({
+            scrollTop: scrollDistance + 'px'
+        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
 
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
-            }
-        });
+        // Hide the menu once clicked if mobile
+        // if ($('header').hasClass('active')) {
+        //     $('header, body').removeClass('active');
+        // }
+    }); 
+});
+    
+    
+    
+    // // Create timeline
+    // $('#experience-timeline').each(function() {
 
-    });
+    //     $this = $(this); // Store reference to this
+    //     $userContent = $this.children('div'); // user content
+
+    //     // Create each timeline block
+    //     $userContent.each(function() {
+    //         $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+    //     });
+
+    //     // Add icons to each block
+    //     $this.find('.vtimeline-point').each(function() {
+    //         $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+    //     });
+
+    //     // Add dates to the timeline if exists
+    //     $this.find('.vtimeline-content').each(function() {
+    //         var date = $(this).data('date');
+    //         if (date) { // Prepend if exists
+    //             $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
+    //         }
+    //     });
+
+    // });
